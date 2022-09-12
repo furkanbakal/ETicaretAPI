32b49987edc40de10cedbc194bb052294766860c
furkanbakal <f.bakal1997@gmail.com>
2022-09-12 23:52:48 +0300

    fixed delete bug

--- Infrastructure/ETicaretServer.Persistence/Contexts/ETicaretServerDbContext.cs
+++ Infrastructure/ETicaretServer.Persistence/Contexts/ETicaretServerDbContext.cs
@@ -26,7 +26,8 @@ namespace ETicaretServer.Persistence.Contexts
                 _ = item.State switch
                 {
                     EntityState.Added => item.Entity.CreatedDate = DateTime.UtcNow,
-                    EntityState.Modified => item.Entity.UpdatedDate = DateTime.UtcNow
+                    EntityState.Modified => item.Entity.UpdatedDate = DateTime.UtcNow,
+                    _=> DateTime.UtcNow
                 };
             }
 
